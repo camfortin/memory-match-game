4fa186a55fabf841c6f79f278a1589e0b097d0cf
Jody Roberts <jodydavidroberts@gmail.com>
2026-03-09 23:30:29 -0400

    closer

--- ios/MemoryMatch/MemoryMatchApp.swift
+++ ios/MemoryMatch/MemoryMatchApp.swift
@@ -8,6 +8,7 @@ struct MemoryMatchApp: App {
         WindowGroup {
             ContentView()
                 .environment(gameState)
+                .preferredColorScheme(.light)
         }
     }
 }
